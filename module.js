3a124b6e5d5cde1b5c69fbb52c46400384693fd8
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2022-01-17 16:07:49 +0530

    Create module.js

---
+++ module.js
@@ -0,0 +1,15 @@
+////Globals - NO WINDOW!!
+
+
+// __dirname - path to current directory
+// __filename - filename
+//require - function to use modules
+///module - info abouth current module 
+//process - info about env where the program is being executed 
+
+
+console.log(`__direname : ${__dirname}`)
+console.log(`__filename : ${__filename}`)
+console.log(`require : ${require}`)
+console.log(`module : ${module}`)
+console.log(`process : ${process}`)
