720a6a29bb4cd73ffb9ca30b52cbd9f358c45d47
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2022-01-19 16:01:55 +0530

    Create Module.js

---
+++ Module.js
@@ -0,0 +1,55 @@
+//Modules
+
+/////every file in node is 
+const {saksham,friends} = require('./module-a')
+const {say} = require('./module-b')
+
+say(saksham)
+
+
+
+
+
+/////////////////////////////////////////names on other file
+
+///local
+const secret ="SUPER SECRET"
+
+
+//global
+const saksham ="saksham"
+const friends = null
+
+module.exports ={saksham, friends}
+// console.log(module); 
+
+// Module {
+//     id: '.',
+//     path: 'c:\\Users\\saksh\\Desktop\\node',
+//     exports: {},
+//     filename: 'c:\\Users\\saksh\\Desktop\\node\\module-a.js',
+//     loaded: false,
+//     children: [],
+//     paths: [
+//       'c:\\Users\\saksh\\Desktop\\node\\node_modules',
+//       'c:\\Users\\saksh\\Desktop\\node_modules',
+//       'c:\\Users\\saksh\\node_modules',
+//       'c:\\Users\\node_modules', 
+//       'c:\\node_modules'
+//     ]
+//   }
+
+
+
+
+
+//////////////////////////////////function on other file
+
+const names = require('./module-a')
+
+const say =(name)=>{
+    console.log(`Hello there ${name}`)
+}
+
+module.exports={say}
+
