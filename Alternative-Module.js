995f0ba837c23a74fae1fc4bd9d4d95966fc88a2
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2022-01-21 00:03:40 +0530

    Create Alternative-Module.js

---
+++ Alternative-Module.js
@@ -0,0 +1,23 @@
+module.exports.items = ['item1','item2']
+const person ={
+    name: 'saksham'
+}
+
+module.exports.single = person
+console.log(person)
+
+
+
+//Modules
+
+// /////every file in node is 
+// const {saksham,friends} = require('./module-a')
+// const {say}= require('./module-b')
+
+// const data = require('./Alternative-flavor')
+
+// console.log(data)
+
+// // say(saksham)
+
+ 
