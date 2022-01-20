997efec871a26a45fe4486356d983538a1aa0bcb
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2022-01-21 00:13:24 +0530

    Create Mind-Grenade.js

---
+++ Mind-Grenade.js
@@ -0,0 +1,40 @@
+////if we require a whole file then at that file the required file will also run at the same time
+
+
+const num1 =5
+const num2 =10
+
+
+
+function addValue(){
+    console.log(`the sum is :: ${num1+num2}`);
+}
+
+
+addValue()
+addValue()
+addValue()
+addValue()
+addValue()
+addValue()
+
+
+
+//Modules
+
+// /////every file in node is 
+// const {saksham,friends} = require('./module-a')
+// const {say}= require('./module-b')
+
+// const data = require('./Alternative-flavor')
+
+// // console.log(data)
+
+// // say(saksham)
+
+
+// require('./Mind-Grenade')
+
+ 
+
+
